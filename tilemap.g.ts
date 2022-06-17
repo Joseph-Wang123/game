d368d0f451fb64632e47a6f7d39e0b4d521c5f00
Joseph-Wang123 <107128342+Joseph-Wang123@users.noreply.github.com>
2022-06-17 10:45:02 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, main.py

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -6,7 +6,26 @@ namespace myTiles {
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`10001000010b020b020b020b020b020b020b02040a09090909090909090909090909090c060909090909090909090909090909080a09090909090909090909090909090c060909090909090909090909090909080a09090909090909090909090909090c060909090909090909090909090909080a09090909090909090909090909090c060909090909090909090909090909080a09090909090909090909090909090c060909090909090909090909090909080a09090909090909090909090909090c060909090909090909090909090909080a09090909090909090909090909090c0609090909090909090909090909090805070707070707070707070707070703`, img`
+            case "level1":return tiles.createTilemap(hex`10001000010b020b020b020b020b020b020b02040a09090909090909090909090909090c060909090909090909090909090909080a0909090909090d090909090909090c06090d0909090909090909090d0909080a090909090d0d0909090d090909090c060909090909090d09090909090909080a0909090d090909090d09090909090c060909090d090909090d0909090909080a090d090d09090d090909090909090c06090909090909090d0909090d0909080a0909090909090d090909090909090c060909090909090d09090909090909080a09090909090909090909090909090c0609090909090909090909090909090805070707070707070707070707070703`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterEast0,sprites.dungeon.floorLight0,sprites.dungeon.greenOuterWest2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterEast2,sprites.dungeon.floorLight1], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -23,7 +42,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterEast0,sprites.dungeon.floorLight0,sprites.dungeon.greenOuterWest2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterEast2], TileScale.Sixteen);
+`, [myTiles.transparency16], TileScale.Sixteen);
         }
         return null;
     })
